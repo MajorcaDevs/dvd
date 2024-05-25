18ea97128d4e1a218c83270de6ae894dcfee85ab
Raúl Blanco <rbr007.movil@gmail.com>
2024-05-25 13:28:12 +0200

    ffmpeg: add -re parameter when live selected

--- dvd/ffmpeg_runner.py
+++ dvd/ffmpeg_runner.py
@@ -4,7 +4,7 @@ import sys
 
 def run_ffmpeg(fps: float, is_live: bool, *args):
     ffmpeg = Popen(
-        ['ffmpeg', '-y', '-f', 'image2pipe', '-c:v', 'bmp', '-r', str(fps), '-i', '-', *args],
+        ['ffmpeg', '-y', *(['-re'] if is_live else []), '-f', 'image2pipe', '-c:v', 'bmp', '-r', str(fps), '-i', '-', *args],
         stdin=PIPE,
         stdout=DEVNULL,
         stderr=DEVNULL,
